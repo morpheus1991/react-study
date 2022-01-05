f277da929116f9185da2c8e1be84e4d38f7b9f9b
조민혁 <chohyuk0725@gmail.com>
2022-01-05 22:33:51 +0900

    찬우피드백

---
+++ 3주차/찬우/day4_solo/src/App.js
@@ -0,0 +1,13 @@
+import IterationSample from './component/IterationSample';
+import PokeAPI from './component/PokeAPI';
+
+function App() {
+  return (
+    <>
+      <IterationSample></IterationSample>
+      <PokeAPI></PokeAPI>
+    </>
+  );
+}
+
+export default App;
